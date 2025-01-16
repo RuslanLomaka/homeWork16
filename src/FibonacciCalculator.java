462f6e5726e78c7f81210992be280976bad2bd3d
margulis <wfptny@gmail.com>
2025-01-16 11:46:30 +0100

    all donr

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
+
+class FibonacciCalculatorTest {
+    public static void main(String[] args) {
+        FibonacciCalculator fc = new FibonacciCalculator();
+        for (int testInput = 10; testInput >= 0; testInput--) {
+            System.out.println("\nfc.calculateRecursive(" + testInput + ") = " + fc.calculateRecursive(testInput));
+            System.out.println("fc.calculateIterative(" + testInput + ") = " + fc.calculateIterative(testInput));
+            System.out.println("fc.calculateDynamically(" + testInput + ") = " + fc.calculateDynamically(testInput));
+        }
+    }
+}
+
 public class FibonacciCalculator {
 
     public int calculateRecursive(int n) {
@@ -8,16 +21,31 @@ public class FibonacciCalculator {
         };
     }
 
+    int[] dpArray = new int[]{0, 1, 1};
+
+    public int calculateDynamically(int n) {
+        if (n <= dpArray.length-2) {
+            return dpArray[n];
+        } else {
+            int previousLength = dpArray.length;
+            dpArray = Arrays.copyOf(dpArray, (n + 1));
+            for (int i = previousLength; i < dpArray.length; i++) {
+                dpArray[i] = dpArray[i - 1] + dpArray[i - 2];
+            }
+            for(int f:dpArray){
+                System.out.print(" " + f);
+            }
+            System.out.println();
+        }
+        return dpArray[n];
+    }
+
     public int calculateIterative(int n) {
         return switch (n) {
             case 0 -> 0;
             case 1, 2 -> 1;
             default -> this.iterate(n);
         };
-    }
-
-    public int calculateDynamically(int n){
-        return n;
     }
 
     private int iterate(int n) {
@@ -30,16 +58,5 @@ public class FibonacciCalculator {
             r = pr + ppr;
         }
         return r;
-    }
-}
-
-class FibonacciCalculatorTest {
-    public static void main(String[] args) {
-        FibonacciCalculator fc = new FibonacciCalculator();
-        for (int testInput = 0; testInput <= 10; testInput++) {
-            System.out.println("\nfc.calculateRecursive(" + testInput + ") = " + fc.calculateRecursive(testInput));
-            System.out.println("fc.calculateIterative(" + testInput + ") = " + fc.calculateIterative(testInput));
-            System.out.println("fc.calculateDynamically(" + testInput + ") = " + fc.calculateDynamically(testInput));
-        }
     }
 }
