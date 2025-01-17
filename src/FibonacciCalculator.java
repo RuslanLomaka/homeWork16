3b492454e7e9ce9a3879b75f99983122801b348c
margulis <wfptny@gmail.com>
2025-01-17 14:14:52 +0100

    all done

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -3,8 +3,13 @@ import java.util.Arrays;
 class FibonacciCalculatorTest {
     public static void main(String[] args) {
         FibonacciCalculator fc = new FibonacciCalculator();
-        for (int testInput = 10; testInput >= 0; testInput--) {
+
+        for (int testInput = 0; testInput <= 11; testInput++) {  //в залежності від обраного ініціалізатора циклу
+     // for (int testInput = 11; testInput >= 0; testInput--) {  //міняється поведінка динамічного методу
+            fc.setRecursiveCallCounter(0);
+            System.out.println();
             System.out.println("\nfc.calculateRecursive(" + testInput + ") = " + fc.calculateRecursive(testInput));
+            System.out.println("Using recursive function " + fc.getRecursiveCallCounter() + " times");
             System.out.println("fc.calculateIterative(" + testInput + ") = " + fc.calculateIterative(testInput));
             System.out.println("fc.calculateDynamically(" + testInput + ") = " + fc.calculateDynamically(testInput));
         }
@@ -12,8 +17,19 @@ class FibonacciCalculatorTest {
 }
 
 public class FibonacciCalculator {
+    public int getRecursiveCallCounter() {
+        return recursiveCallCounter;
+    }
+
+    public void setRecursiveCallCounter(int recursiveCallCounter) {
+        this.recursiveCallCounter = recursiveCallCounter;
+    }
+
+    private int recursiveCallCounter;
 
     public int calculateRecursive(int n) {
+        recursiveCallCounter += 1;
+        System.out.print("*"); //візуалізує в консолі загальну к-ть рекурсивних викликів методу для заданого n
         return switch (n) {
             case 0 -> 0;
             case 1, 2 -> 1;
@@ -24,7 +40,7 @@ public class FibonacciCalculator {
     int[] dpArray = new int[]{0, 1, 1};
 
     public int calculateDynamically(int n) {
-        if (n <= dpArray.length-2) {
+        if (n <= dpArray.length - 2) {
             return dpArray[n];
         } else {
             int previousLength = dpArray.length;
@@ -32,8 +48,9 @@ public class FibonacciCalculator {
             for (int i = previousLength; i < dpArray.length; i++) {
                 dpArray[i] = dpArray[i - 1] + dpArray[i - 2];
             }
-            for(int f:dpArray){
-                System.out.print(" " + f);
+            System.out.println("Recalculating dynamic programming array");
+            for (int f : dpArray) {
+                System.out.print(" " + f); //динамічний масив виводиться тільки коли він перераховується
             }
             System.out.println();
         }
